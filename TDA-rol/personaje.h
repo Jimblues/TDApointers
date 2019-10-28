3c41b2640b88c008dbcab706b893105a7571413e
Jimblues <56451199+Jimblues@users.noreply.github.com>
2019-10-28 12:19:03 -0300

    Update personaje.h

--- TDA-rol/personaje.h
+++ TDA-rol/personaje.h
@@ -6,6 +6,13 @@ typedef struct Character *Personaje;
 
 Personaje crearPersonaje();
 void mostrarPersonaje(Personaje p);
+char* getNombrePersonaje(Personaje p);
+int getAtkPersonaje(Personaje p);
+int getHPPersonaje(Personaje p);
 
+void setNombrePersonaje(Personaje p,char* nombre);
+void setAtkPersonaje(Personjae p,int atk);
+void setHPPersonaje(Personaje p,int hp);
 
+void destruirPersonaje(Personaje p);
 #endif // PERSONAJE_H_INCLUDED
